1f8e162b6a2c5d0347eb8b9d28912259d6ca5e7d
Hani Gerges <hanigerges@our-edu.net>
2023-10-29 00:14:06 +0200

    feat : using model for ts

---
+++ models/userModel.ts
@@ -0,0 +1,11 @@
+export interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  mobile: string;
+  birthdate: string;
+  address: string;
+  gender: Number;
+}
