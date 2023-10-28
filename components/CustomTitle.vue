17ad55e4dec61a0ba44da0b761f05fe1b2418cb2
Hani Gerges <hanigerges@our-edu.net>
2023-10-29 00:15:44 +0200

    feat : custom header

---
+++ components/CustomTitle.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="m-2">
+    <h1>
+      {{ $t(`home.${title}`) }}
+    </h1>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+<style lang="scss" scoped>
+h1 {
+  font-family: sans-serif;
+  font-size: 2rem;
+  font-weight: bold;
+  margin-top: 0;
+  margin-bottom: 0.5rem;
+}
+</style>
