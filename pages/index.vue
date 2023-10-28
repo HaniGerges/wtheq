f3c6d87cffb557cc38a19c25fb6e3a563e6f3fb8
Hani Gerges <hanigerges@our-edu.net>
2023-10-29 00:12:10 +0200

    feat : creating pages and ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,80 @@
 <template>
+  <div class="md:w-full lg:w-1/2 w-full mx-auto  mt-12">
+    <div
+      class="bg-white p-3 pb-4 h-max shadow-sm rounded-sm border-2 border-gray-100 shadow-lg rounded-xl"
+    >
+      <div class="flex justify-center space-x-2 rounded-full mb-4">
+        <img
+          src="@/assets/img/profile.webp"
+          class="w-24 h-24 rounded-full border-2 border-gray-100 hover:border-gray-300 transition duration-300 ease-in-out"
+          alt=""
+        />
+      </div>
+      <div class="text-gray-700">
+        <div
+          class="grid md:grid-cols-2 grid-cols-1 text-sm whitespace-pre-wrap"
+        >
+          <div class="grid md:grid-cols-2 grid-cols-1">
+            <div class="px-2 py-2 font-semibold">{{$t(`home.firstName`)}}</div>
+            <div class="px-2 py-2">{{ userModel.firstName }}</div>
+          </div>
+          <div class="grid md:grid-cols-2 grid-cols-1">
+            <div class="px-2 py-2 font-semibold">{{$t(`home.lastName`)}}</div>
+            <div class="px-2 py-2">{{ userModel.lastName }}</div>
+          </div>
+          <div class="grid md:grid-cols-2 grid-cols-1">
+            <div class="px-2 py-2 font-semibold">{{$t(`home.gender`)}}</div>
+            <div class="px-2 py-2">{{ userModel.gender }}</div>
+          </div>
+          <div class="grid md:grid-cols-2 grid-cols-1">
+            <div class="px-2 py-2 font-semibold">{{$t(`home.mobile`)}}</div>
+            <div class="px-2 py-2">
+              <a :href="`tel:${userModel.mobile}`"> {{ userModel.mobile }}</a>
+            </div>
+          </div>
+          <div class="grid md:grid-cols-2 grid-cols-1">
+            <div class="px-2 py-2 font-semibold">{{$t(`home.address`)}}</div>
+            <div class="px-2 py-2">{{ userModel.address }}</div>
+          </div>
+          <div class="grid md:grid-cols-2 grid-cols-1">
+            <div class="px-2 py-2 font-semibold">{{$t(`home.email`)}}</div>
+            <div class="px-2 py-2">
+              <a class="text-blue-800" :href="`mailto:${userModel.email}`">{{
+                userModel.email
+              }}</a>
+            </div>
+          </div>
+          <div class="grid md:grid-cols-2 grid-cols-1">
+            <div class="px-2 py-2 font-semibold">{{$t(`home.birthdate`)}}</div>
+            <div class="px-2 py-2">{{ userModel.birthdate }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-
+import Vue from "vue";
+import { User } from "@/models/userModel";
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+  layout: "default",
+  data() {
+    return {
+      userModel: {
+        id:"123-456-789",
+        firstName: "Hani",
+        lastName: "Gerges",
+        email: "hany.grges@gmail.com",
+        password:"122",
+        mobile: "+201208950563",
+        birthdate: "Dec 10, 1994",
+        address: "Cairo, El-Zytoon",
+        gender: 0, //0 for male, 1 for female
+      } as User,
+    };
+  },
+  created() {},
+});
 </script>
---
+++ pages/payment.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="">
+    <dropIn
+      class="md:w-full lg:w-1/2 w-full mx-auto px-4 mt-12"
+      :dropInConfigs="dropInConfigs"
+      :enablePayPal="enablePayPal"
+    >
+    </dropIn>
+  </div>
+</template>
+
+<script lang="ts">
+import dropIn from "@/components/Nav/payments/dropIn.vue";
+
+export default {
+  name: "Payment",
+  layout: "default",
+  components: {
+    dropIn,
+  },
+  data() {
+    return {
+      dropInConfigs: {
+        authorization:
+          "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiJkOWU4ZjQ3MDkwYjVhZGRhMDNhNjAyMTUzNTdmNDhmYWQ3ZGJhZTYxNWNhMWE1MGU1ZmM2ZGJlNTZjMGZkZWZlfGNyZWF0ZWRfYXQ9MjAxNy0wOC0wOVQwNzowNjoyNi4xNjU5NzU0MzgrMDAwMFx1MDAyNmN1c3RvbWVyX2lkPTg5NTQ0NTUwM1x1MDAyNm1lcmNoYW50X2lkPXFwZG5id25qYnJ3MjdrbWpcdTAwMjZwdWJsaWNfa2V5PTM3cmY1ZmR5M2h2a3Z6NDYiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvcXBkbmJ3bmpicncyN2ttai9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbInBvc3RhbF9jb2RlIl0sImVudmlyb25tZW50Ijoic2FuZGJveCIsImNsaWVudEFwaVVybCI6Imh0dHBzOi8vYXBpLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb206NDQzL21lcmNoYW50cy9xcGRuYnduamJydzI3a21qL2NsaWVudF9hcGkiLCJhc3NldHNVcmwiOiJodHRwczovL2Fzc2V0cy5icmFpbnRyZWVnYXRld2F5LmNvbSIsImF1dGhVcmwiOiJodHRwczovL2F1dGgudmVubW8uc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbSIsImFuYWx5dGljcyI6eyJ1cmwiOiJodHRwczovL2NsaWVudC1hbmFseXRpY3Muc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbS9xcGRuYnduamJydzI3a21qIn0sInRocmVlRFNlY3VyZUVuYWJsZWQiOnRydWUsInBheXBhbEVuYWJsZWQiOnRydWUsInBheXBhbCI6eyJkaXNwbGF5TmFtZSI6IldoaWxkIENhdWdodCIsImNsaWVudElkIjoiQVRsYnV5blItQkg4b0F0c0ZiTFZTdXk4YUpMSjQ4cF9hVXBMNVQyY1dRdXAxRk80bV90RlFEdjM4QXpPQjNEbFBFc1FGOFVGQkF4SC1BclYiLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjpmYWxzZSwiZW52aXJvbm1lbnQiOiJvZmZsaW5lIiwidW52ZXR0ZWRNZXJjaGFudCI6ZmFsc2UsImJyYWludHJlZUNsaWVudElkIjoibWFzdGVyY2xpZW50MyIsImJpbGxpbmdBZ3JlZW1lbnRzRW5hYmxlZCI6dHJ1ZSwibWVyY2hhbnRBY2NvdW50SWQiOiJ3aGlsZGNhdWdodCIsImN1cnJlbmN5SXNvQ29kZSI6IlVTRCJ9LCJtZXJjaGFudElkIjoicXBkbmJ3bmpicncyN2ttaiIsInZlbm1vIjoib2ZmIn0=",
+        selector: "#dropin-container",
+        styles: {
+          direction: this.$i18n.locale == "ar" ? "rtl" : "ltr" as string,
+        },
+        locale: this.$i18n.locale ?? "en_US",
+        vaultManager: true,
+        card: {
+          vault: {
+            allowVaultCardOverride: false,
+            vaultCard: true,
+          },
+          cardholderName: {
+            required: true,
+          },
+        },
+      },
+      enablePayPal: false,
+    };
+  },
+  methods: {
+    btHFError(message: string) {
+      console.error(message);
+      // do something with the error message
+    },
+    btHFPayload(payload: any) {
+      console.log(payload);
+      // do something with the token payload
+    },
+    submitTransaction() {
+      this.$emit("tokenize");
+    },
+  },
+  created() {
+    this.submitTransaction();
+  },
+};
+</script>
